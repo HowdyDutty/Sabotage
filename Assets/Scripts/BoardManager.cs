a790048c550c0930efaba70382a4f897950c1dc8
Roman Larionov <romanylarionov@gmail.com>
2014-05-30 00:28:11 -0400

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+	public GameObject tile;
+	public int gridWidth   = 10;
+	public int gridHeight  = 10;
+
+	private int tileWidth  = 116;
+	private int tileHeight = 116;
+
+	private Transform myTransform;
+	private Quaternion myRotation;
+
+	void Start () 
+	{
+		myTransform = this.transform;
+		myRotation = this.transform.rotation;
+		createBoard();
+	}
+
+	void createBoard() 
+	{
+		List<GameObject> tiles = new List<GameObject>();
+		Vector3 currTileLocation = new Vector3(0, 0, 0);
+		tile.transform.localScale = new Vector3(tileWidth, 1, tileHeight);
+
+		for (int i = -10; i < gridWidth/2; i++)
+		{	
+			for (int j = -10; j < gridHeight/2; j++)
+			{
+				GameObject currTile = (GameObject)Instantiate(tile, currTileLocation, Quaternion.Euler(270, 0, 0));
+				tiles.Add(currTile);
+				currTileLocation.y += j;
+			}
+			currTileLocation.x += i;
+			currTileLocation.y = 0;
+		}
+	}
+} // BoardManager
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private BoardManager boardManagerScript;
+
+	void Start () {
+		boardManagerScript = GameObject.FindGameObjectWithTag("GameManager").GetComponent<BoardManager>();
+
+
+	}
+	
+	void Update () {
+	
+	}
+}
