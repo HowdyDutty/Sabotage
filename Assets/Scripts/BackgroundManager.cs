f49641663ba2ce18d5db30380bf9bec43c2d8d31
Roman Larionov <romanylarionov@gmail.com>
2014-06-24 15:57:25 -0400

    Set the background to static batching, changed names of pieces.

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -14,22 +14,19 @@ public class BackgroundManager : MonoBehaviour
 
 	private void createBackground()
 	{
-		// Bottom
-		createPiece(new Vector3(0, 0, 10), new Vector3(0, 0, 0));
-		// Left
-		createPiece(new Vector3(-10, 0, 0), new Vector3(0, 270, 0));
-		// Right
-		createPiece(new Vector3(10, 0, 0), new Vector3(0, 90, 0));
-		// Back
-		createPiece(new Vector3(0, 10, 0), new Vector3(270, 0, 0));
+		createPiece(new Vector3(0, 0, 10), new Vector3(0, 0, 0), "Bottom Piece");
+		createPiece(new Vector3(-10, 0, 0), new Vector3(0, 270, 0), "Left Piece");
+		createPiece(new Vector3(10, 0, 0), new Vector3(0, 90, 0), "Right Piece");
+		createPiece(new Vector3(0, 10, 0), new Vector3(270, 0, 0), "Back Piece");
 	}
 
-	private void createPiece(Vector3 position, Vector3 rotation)
+	private void createPiece(Vector3 position, Vector3 rotation, string name)
 	{
 		GameObject instance = (GameObject)Instantiate(quad);
 		instance.renderer.material.color = Color.black;
 		instance.transform.position = position;
 		instance.transform.localScale = new Vector3(20, 20, 0);
 		instance.transform.rotation = Quaternion.Euler(rotation);
+		instance.name = name;
 	}
 }
