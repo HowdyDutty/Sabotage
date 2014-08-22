ab012a1b85193acdb9e3afa579d76afb545f9a95
Roman Larionov <romanylarionov@gmail.com>
2014-08-21 21:21:33 -0400

    Updated the way pick ups are spawned to a simpler method.

--- Assets/Scripts/Managers/PickUpManager.cs
+++ Assets/Scripts/Managers/PickUpManager.cs
@@ -9,37 +9,38 @@ public class PickUpManager : MonoBehaviour
 	private GameObject pickUp;
 	public List<PickUpItem> pickUpList { get; private set; }
 
-	public int numPickUps 	 = 30;
+	private int maxNumberPickUps = 4;	// Per round.
+	public int totalPickUps;
 
 	void Start() 
 	{
 		boardManagerScript = FindObjectOfType<BoardManager>();
 		pickUp = (GameObject)Resources.Load("Prefabs/Pick Ups/Temporary");	// Change from temp Prefab in future.
 		pickUpList = new List<PickUpItem>();
+		totalPickUps = 0;
 
 		createPickUps();
 	}
 
 	public void createPickUps()
 	{
-		int pickUpsLeft = 1;
-		foreach (Tile t in boardManagerScript.tiles)
-		{	
-			if (pickUpsLeft >= numPickUps)
-			{
-				break;
-			}
-			else if (Random.Range(1, boardManagerScript.tiles.Count) == t.tileNumber)
-			{	
-				GameObject currPickUpItem = (GameObject)Instantiate(pickUp, t.position, t.tile.transform.rotation);
-				currPickUpItem.collider.isTrigger = true;
-				currPickUpItem.transform.localScale = new Vector3(.3f, .3f, .3f);
-				currPickUpItem.renderer.material.color = Color.cyan;
-				
-				PickUpItem currItem = new PickUpItem(currPickUpItem, t, pickUpsLeft);
-				pickUpList.Add(currItem);
-				pickUpsLeft++;
-			}
+		int randomNumberPickUps = Random.Range(0, maxNumberPickUps);
+		Tile[] tiles = boardManagerScript.tiles.ToArray();
+
+		for (int i = 0; i < randomNumberPickUps; i++)
+		{
+			int randomTilePlacement = Random.Range(2, boardManagerScript.tiles.Count);
+			Tile placementTile = tiles[randomTilePlacement];
+		
+			GameObject currPickUpItem = (GameObject)Instantiate(pickUp, placementTile.position, placementTile.tile.transform.rotation);
+			currPickUpItem.collider.isTrigger = true;
+			currPickUpItem.transform.localScale = new Vector3(.3f, .3f, .3f);
+			currPickUpItem.renderer.material.color = Color.cyan;
+
+			totalPickUps++;
+
+			PickUpItem currItem = new PickUpItem(currPickUpItem, placementTile, totalPickUps);
+			pickUpList.Add(currItem);
 		}
 	}
 }
